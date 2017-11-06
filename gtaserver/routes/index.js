a1665596fce0f24e4c4a89476f00cf1937cc20bf
jhynes54 <jillian@Jillians-MacBook-Air.local>
2017-11-06 18:53:49 -0500

    Added Database

--- gtaserver/routes/index.js
+++ gtaserver/routes/index.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/GTADatabase";
 var express = require('express');
 var router = express.Router();
 
@@ -11,6 +13,15 @@ router.post('/', function(req, res, next) {
 
     var user = {'first name' : req.body.firstName, 'last name': req.body.lastName, 'email': req.body.email,
         'password' : req.body.password, 'confirm password': req.body.confirmPassword};
+
+    MongoClient.connect(url, function(err, db) {
+     if (err) throw err;
+     db.collection("CredentialCollection").insertOne(user, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      })
+    });
 
     console.log(user);
 
