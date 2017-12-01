de0596d75d6a19519c8a3158fbda643ffcbbeed5
Jill Hynes <jlhynes@umass.edu>
2017-12-01 18:15:45 -0500

    Added sign in feature

--- gtaserver/routes/index.js
+++ gtaserver/routes/index.js
@@ -4,32 +4,65 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/signin', function(req, res, next) {
   res.render('index', { title: 'Graphical Tagging Application' });
 
 });
 
-router.post('/', function(req, res, next) {
+router.post('/signin', function(req, res, next) {
 
-    var user = {'first name' : req.body.firstName, 'last name': req.body.lastName, 'email': req.body.email,
+//Sign in page
+var correct = false;
+  MongoClient.connect(url, function(err, db) {
+    db.collection('CredentialCollection', function(err, collection) {
+      collection.findOne({'username' : req.body.username}, function(err, doc) {
+        console.log(doc);
+        if(doc){
+          console.log("Username Exists!");
+	        if((doc.password) == (req.body.password)){
+            console.log("Password works!!!");
+            return res.redirect('home');
+          } else{
+            console.log("Password incorrect!");
+            return res.redirect('signin');
+            }
+        } else{
+          return res.redirect('signin');
+        }
+       });
+
+    // our collection has returned, now we can close the database
+       db.close();
+    });
+  });
+});
+
+
+
+
+
+//Sign up page
+
+router.post('/signup', function(req, res, next) {
+  var user = {'first name' : req.body.firstName, 'last name': req.body.lastName, 'username' : req.body.username, 'email': req.body.email,
         'password' : req.body.password, 'confirm password': req.body.confirmPassword};
 
-    MongoClient.connect(url, function(err, db) {
-     if (err) throw err;
-     db.collection("CredentialCollection").insertOne(user, function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-      })
-    });
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db.collection("CredentialCollection").insertOne(user, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    })
+  });
 
-    console.log(user);
+  console.log(user);
 
-    res.redirect('home');
+  res.redirect('home');
 });
 
 router.get('/home', function(req, res, next) {
-   res.render('home');
+  res.render('home');
 });
 
 
