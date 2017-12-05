4f0f86c5873b1f74c6933da85188390bec74745e
stephenren <cheeryparadox@gmail.com>
2017-12-04 21:35:37 -0500

    ui chances

--- gtaserver/routes/index.js
+++ gtaserver/routes/index.js
@@ -37,6 +37,7 @@ router.post('/signin', function(req, res, next) {
 	        if((doc.password) == (req.body.password)){
             console.log("Password works!");
             req.session.firstName = doc.firstName;
+            req.session.lastName = doc.lastName;
             return res.redirect('home');
 
           } else{
@@ -76,109 +77,91 @@ router.post('/signup', function(req, res, next) {
   res.redirect('home');
 });
 
+
+
+//my code looks cleaner
 router.get('/home', function(req, res, next) {
-
-  MongoClient.connect(url, function(err, db){
-      console.log("made it asdfasd")
-      if(err){
-          console.log("connection error");
-          throw err;
-      }
-      //if we're connected to the database...
-      else{
-          console.log("connection success");
-
-
-
-              db.collection(collection).find().toArray(function(err, results) {
-                  if(err){
-                      console.log("collection error");
-                      throw err;
-                  }
-                  else{
-
-
-                      // console.log(results);
-                      var keys = Object.keys(results[0]);
-                      var key = [keys.length-1];
-                      var j = 0;
-                      for(var i = 1 ; i < Object.keys(results[0]).length ; i++){
-                          key[j]=keys[i];
-                          j++;
-                      }
-
-                      holder = [];
-                      for(obj in results){
-                          temp = {};
-                          for(name in key){
-                              temp[key[name]]=results[obj][key[name]];
-                          }
-                          holder[obj]=temp;
-                          temp={};
-                      }
-
-                      idHolder=[];
-                      for (obj in results){
-                          idHolder[obj]=results[obj]["_id"];
-                      }
-                      console.log(req.session);
-                      res.render('home', { test: req.session.firstName, keys:key, data:results} )
-                  }
-              });
-
-      }
-  });
-});
-
-
-
-
-router.post('/home', function(req,res, next){
-
-    console.log("made it")
-
-    console.log(req.params.id);
-
-    var newObj = req.body;
-
-    console.log(newObj);
-
-
-
-    MongoClient.connect(url, function(err,db){
+    console.log("/home: made it")
+    MongoClient.connect(url, function(err, db){
         if(err){
+            console.log("/home: connection error");
             throw err;
         }
         else{
-            console.log("connection good");
-            db.collection(collection).find().toArray(function(err,results){
+            console.log("/home: connection success");
+            db.collection(collection).find().toArray(function(err, results) {
                 if(err){
+                    console.log("/home: collection not found");
                     throw err;
                 }
                 else{
-                    console.log("collection good");
+                    console.log("/home: collection found")
+                    var keys = Object.keys(results[0]);
+                    var key = [keys.length-1];
+                    var j = 0;
+                    for(var i = 1 ; i < Object.keys(results[0]).length ; i++){
+                        key[j]=keys[i];
+                        j++;
+                    }
+
+                    //yes it makes that much of a difference to me.
+                    var Fname = req.session.firstName;
+                    var Lname = req.session.lastName;
+
+                    Fname = Fname.charAt(0).toUpperCase() + Fname.slice(1);
+                    Lname = Lname.charAt(0).toUpperCase() + Lname.slice(1);
+
+                    var capitalized = Fname+" "+Lname;
+
+                    console.log(capitalized)
+
+                    res.render('home', { UserName:capitalized, keys:key, data:results} )
+                }
+            });
+        }
+    });
+});
+
+//this handles updating the systems from the front end
+router.post('/dbupdate', function(req,res, next){
+    console.log("/dbupdate: made it")
+    var newObj = req.body;
+
+    console.log(newObj)
+    MongoClient.connect(url, function(err,db){
+        if(err){
+            console.log("/dbupdate: connection error");
+            throw err;
+        }
+        else{
+            console.log("/dbupdate: connection good");
+            db.collection(collection).find().toArray(function(err,results){
+                if(err){
+                    console.log("/dbupdate: collection not found");
+                    throw err;
+                }
+                else{
+                    console.log("/dbupdate: collection found");
                     for(obj in results){
                         var oldObj = results[obj];
                         if(oldObj["_id"].equals(newObj["_id"])){
-                            console.log("yay");
 
                             delete newObj["_id"];
-                            console.log(newObj);
+                            delete newObj["checked"]
 
                             db.collection(collection).updateOne(oldObj,newObj,function(err,res){
                                 if(err){
+                                    console.log("/dbupdate: collection update error");
                                     throw err;
                                 }
                                 else{
-                                    console.log("1 document updated");
+                                    console.log("/dbupdate: 1 document updated");
                                 }
                             })
-
                         }
                     }
                 }
             })
-
         }
     })
     return res.redirect('home');
