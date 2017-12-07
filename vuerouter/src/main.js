6647c3d8f46fdc2814984d0a2cb9402d2e959bef
flogan <flogan@umass.edu>
2017-12-07 13:52:18 -0500

    compnents and new item.vue

--- vuerouter/src/main.js
+++ vuerouter/src/main.js
@@ -1,7 +1,7 @@
 // main.js
 
 import Vue from 'vue';
-
+import CreateItem from './components/CreateItem.vue';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
@@ -9,20 +9,13 @@ import VueAxios from 'vue-axios';
 import axios from 'axios';
 Vue.use(VueAxios, axios);
 
+const router = new VueRouter({ mode: 'history', routes: routes});
+new Vue(Vue.util.extend({ router })).$mount('#app');
 
 const routes = [
     {
-        name: 'CreateItem',
-        path: '/',
-        component: CreateItem
-    }
-]
-
-const router = new VueRouter({ mode: 'history', routes: routes});
-
-new Vue(Vue.util.extend({ router })).$mount('#app');
-
-import App from './App.vue';
-new Vue(Vue.util.extend({ router }, App)).$mount('#app');
-
-import CreateItem from './components/CreateItem.vue';
+          name: 'CreateItem',
+          path: '/',
+          component: CreateItem
+      }
+  ];
