c6070f28e0c3739912640b3bc891e57c0de94fc8
Ryan Mahan <ryanmahan97@gmail.com>
2017-12-07 13:45:46 -0500

    Created links in main.js with CreateItem.vue

--- vuerouter/src/main.js
+++ vuerouter/src/main.js
@@ -9,5 +9,20 @@ import VueAxios from 'vue-axios';
 import axios from 'axios';
 Vue.use(VueAxios, axios);
 
-const router = new VueRouter({ mode: 'history' });
+
+const routes = [
+    {
+        name: 'CreateItem',
+        path: '/',
+        component: CreateItem
+    }
+]
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
 new Vue(Vue.util.extend({ router })).$mount('#app');
+
+import App from './App.vue';
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+
+import CreateItem from './componenets/CreateItem.vue';
