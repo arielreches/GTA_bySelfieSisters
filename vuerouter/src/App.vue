e7b9b02c87eb7f040679b979ba7dd68d82c17e7f
Ryan Mahan <ryanmahan97@gmail.com>
2017-12-07 21:00:38 -0500

    ITS ALIIIIVE

--- vuerouter/src/App.vue
+++ vuerouter/src/App.vue
@@ -1,24 +1,22 @@
-// App.vue
-
-
 <template>
-    <div class="container">
-        <div>
-            <transition name="fade">
-                <router = view></router = view>
-            </transition>
-        </div>
-    </div>
+  <b-container>
+    <h1>MEVN Stack</h1>
+    <router-view/>
+  </b-container>
 </template>
-<style>
-    .fade-enter-active, .fade-leave-active {
-      transition: opacity .5s
-    }
-    .fade-enter, .fade-leave-active {
-      opacity: 0
-    }
-</style>
+
 <script>
-    export default{
-    }
+export default {
+  name: 'app'
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
