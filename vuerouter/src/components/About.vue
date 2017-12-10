3c900fc693f59481189c73debc9b82e07c668fb0
stephenren <cheeryparadox@gmail.com>
2017-12-10 16:55:37 -0500

    made the things at the top of the app.vue page do things now

---
+++ vuerouter/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div>
+        <img src="https://cdn.dribbble.com/users/135467/screenshots/3480075/purrito-generic.gif">
+    </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script>
+
+</script>
