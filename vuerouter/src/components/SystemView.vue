a420b4443893fa61ad28420ef8809b7bf0ddd542
Jill Hynes <jlhynes@umass.edu>
2017-12-13 16:28:06 -0500

    Fixed tag redirect and created tag model

--- vuerouter/src/components/SystemView.vue
+++ vuerouter/src/components/SystemView.vue
@@ -58,7 +58,7 @@ export default {
     addTag (tag) {
       this.system.Tag.push(tag)
       axios.put('http://localhost:3000/system/' + this.$route.params.id, this.system)
-      this.$router.push('home')
+      this.$router.push('grouplist')
     }
   }
 }
