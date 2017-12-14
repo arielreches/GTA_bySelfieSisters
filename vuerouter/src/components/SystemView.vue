c840f1cf98ed86c63035b7a9fcc5f8a789376d2e
Quinn Jackson <qjackson@umass.edu>
2017-12-13 23:58:29 -0500

    Added more system attributes

--- vuerouter/src/components/SystemView.vue
+++ vuerouter/src/components/SystemView.vue
@@ -2,7 +2,7 @@
   <b-row>
     <b-col cols="12">
       <h2>
-        {{system.systemName}} System Details 
+        {{system.systemName}} System Details
         <b-btn variant='primary' size='sm' @click.stop="back()">Back to Group</b-link>
       </h2>
       <b-jumbotron>
@@ -10,35 +10,65 @@
           {{system.systemName}}
         </template>
         <template slot="lead">
-          Company Name: {{system.companyName}}<br>
-          System Model: {{system.model}}<br>
-          OS Version: {{system.osVersion}}<br>
-          Recommended OS Version: {{system.recommended.osVersion}}<br>
-          Common Provisional Groups (CPGs): {{system.cpgCount}}<br>
-          Country/Region of Origin: {{system.location.region + '/' + system.location.country}}<br>
-          Capacity Percentage(Raw Storage): {{parseInt(system.capacity.total.freePct) + '%' + ' (' + parseInt(system.capacity.total.freeTiB) + ' TiB)'}}<br>
-          Tags:<br>
-          
-          <b-button v-for="item in system.Tag" size="small" variant="outline-success" :key="item.id">
+          <hr class="my-4">
+          <!-- <b-btn variant="success" @click.stop="addtoGroup(system._id)">Add to Group</b-btn> -->
+          <b-form @submit="addTag(tag)">
+            <b-form-input v-model.trim="tag" type="text" placeholder="Add Tag...">
+            </b-form-input>
+            <b-btn type="submit" class="addTagButton">Add Tag</b-btn>
+          </b-form>
+          <span class="attribute">Tags:</span> <b-button v-for="item in system.Tag" size="small" variant="outline-success" :key="item.id" class = "tagButton">
             {{item}}
-          </b-button>
+          </b-button><br>
+          <span class="attribute">Company Name:</span> {{system.companyName}}<br>
+          <span class="attribute">System Model:</span> {{system.model}}<br>
+          <span class="attribute">OS Version:</span> {{system.osVersion}}<br>
+          <span class="attribute">Recommended OS Version:</span> {{system.recommended.osVersion}}<br>
+          <span class="attribute">Common Provisional Groups (CPGs):</span> {{system.cpgCount}}<br>
+          <span class="attribute">Country/Region of Origin:</span> {{system.location.region + '/' + system.location.country}}<br>
+          <span class="attribute">Capacity Percentage (Raw Storage):</span> {{parseInt(system.capacity.total.freePct) + '%' + ' (' + parseInt(system.capacity.total.freeTiB) + ' TiB)'}}<br>
           <template v-if='system.writeServiceTimeMillis != null'>
-          Write Service Time: {{system.writeServiceTimeMillis}}
+            <span class="attribute">Write Service Time:</span> {{system.writeServiceTimeMillis}}
           </template>
+          <span class="attribute">Updated:</span> {{system.updated}}<br>
+          <span class="attribute">Count/Node Count Offline:</span> {{system.nodes.nodeCount + '/' + system.nodes.nodeCountOffline}}<br>
+          <span class="attribute">Disc Count:</span> {{system.disks.total.diskCount}}<br>
+          <span class="attribute">Disc Count Disk Normal:</span> {{system.disks.total.diskCountDiskNormal}}<br>
+          <span class="attribute">Disc Count Degraded:</span> {{system.disks.total.diskCountDegraded}}<br>
+          <span class="attribute">Disc Count Failed:</span> {{system.disks.total.diskCountFailed}}<br>
+          <span class="attribute">Contract Start Date:</span> {{system.contractStartDate}}<br>
+          <span class="attribute">Contract End Date:</span> {{system.contractEndDate}}<br>
+          <span class="attribute">SP Version:</span> {{system.sp.spVersion}}<br>
+          <span class="attribute">VV Count:</span> {{system.vvCount}}<br>
+          <span class="attribute">TPVV Count:</span> {{system.tpvvCount}}<br>
+          <span class="attribute">VV Count Full:</span> {{system.vvCountFull}}<br>
+          <span class="attribute">TDVV Size TB:</span> {{system.tdvvSizeTB}}<br>
+          <span class="attribute">Bandwidth (KB/s):</span> {{system.performance.portBandwidthData.total.dataRateKBPSAvg}}<br>
+          <span class="attribute">Total Service Time (Millis):</span> {{system.performance.summary.portInfo.totalServiceTimeMillis}}<br>
+          <span class="attribute">Read Service Time (Millis):</span> {{system.performance.summary.portInfo.readServiceTimeMillis}}<br>
+          <span class="attribute">Write Service Time (Millis):</span> {{system.performance.summary.portInfo.writeServiceMillis}}<br>
+          <span class="attribute">Del Ack Percent:</span> {{system.delAckPct}}<br>
+          <span class="attribute">Read Bandwidth (MB/s):</span> {{system.performance.summary.vvInfo.vvsByType.ssd.readBandwidthMBPS}}<br>
+          <span class="attribute">Write Bandwidth (MB/s):</span> {{system.performance.summary.vvInfo.vvsByType.ssd.writeBandwidthMBPS}}<br>
         </template>
-
-        <hr class="my-4">
-        <!-- <b-btn variant="success" @click.stop="addtoGroup(system._id)">Add to Group</b-btn> -->
-        <b-form @submit="addTag(tag)">
-          <b-form-input v-model.trim="tag" type="text" placeholder="Add new Tag">
-          </b-form-input>
-          <br>
-          <b-btn type="submit">Add Tag</b-btn>
-        </b-form>
       </b-jumbotron>
     </b-col>
   </b-row>
 </template>
+
+<style>
+  .attribute {
+    font-weight: bold;
+  }
+  .tagButton {
+    margin-right: 2px;
+  }
+  .addTagButton {
+    margin-top: 5px;
+    margin-bottom: 20px;
+    cursor:pointer;
+  }
+</style>
 
 <script>
 import axios from 'axios'
